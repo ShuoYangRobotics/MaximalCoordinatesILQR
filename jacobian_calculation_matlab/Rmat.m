469d7cb19de6f02bad07265ee794f324f7a03a35
Shuo <paulyang1990@gmail.com>
2021-04-09 21:10:31 -0400

    add matlab jacobian calculation script

---
+++ jacobian_calculation_matlab/Rmat.m
@@ -0,0 +1,8 @@
+function mat = Rmat(q)
+mat = [q(1) -q(2) -q(3) -q(4);
+       q(2)  q(1)  q(4) -q(3);
+       q(3) -q(4)  q(1)  q(2);
+       q(4)  q(3) -q(2)  q(1)];
+   
+end
+
---
+++ jacobian_calculation_matlab/TEST.m
@@ -0,0 +1,23 @@
+syms qw qv1 qv2 qv3 real
+q = [qw;qv1;qv2;qv3]
+Lmat(q)
+H = [0 0 0;1 0 0; 0 1 0; 0 0 1]
+V = [0 0 0;1 0 0; 0 1 0; 0 0 1]'
+syms p1 p2 p3 real
+p = [p1;p2;p3]
+% test the jacobian of translation joint q*p
+g = V*Rmat(q)'*Lmat(q)*H*p
+jacobian(g,q)
+2*V*Rmat(q)'*Rmat([0;p])
+2*V*Rmat(q)'*Rmat([0;p])-jacobian(g,q)
+
+% test the jacobian of rotate joint
+syms aw av1 av2 av3 real
+qa = [aw;av1;av2;av3]
+syms bw bv1 bv2 bv3 real
+qb = [bw;bv1;bv2;bv3]
+cmat = [0 1 0; 
+        1 0 0];
+g2 = cmat * V*Lmat(qa)'*qb
+jacobian(g2,qa)
+jacobian(g2,qb)
