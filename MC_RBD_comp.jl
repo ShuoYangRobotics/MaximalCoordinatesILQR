90fff6e188debed60b1cc23b6507c973b2e3d820
DengYuelin <dengyuelin66dyl@163.com>
2021-05-01 15:51:08 -0400

    fix bug and generalized code

--- MC_RBD_comp.jl
+++ MC_RBD_comp.jl
@@ -8,9 +8,81 @@ Tf = 0.5
 dt = 0.005
 N = Int(Tf/dt)
 
+"""Build MC model"""
+model = FloatingSpaceOrth(3)
+
+"""Build RBD model"""
+function generate_RBD_model(model::FloatingSpace)
+    nb = model.nb # the number of arms
+    # define zero gravity world
+    world = RBD.RigidBody{Float64}("world")
+    FloatingSpaceRobot = RBD.Mechanism(world; gravity=[0., 0., 0.])
+
+    joint0 = RBD.Joint("joint0", RBD.QuaternionFloating{Float64}()) # joint connecting base to world
+    # inertia frame of base
+    base_inertia_frame = RBD.SpatialInertia(
+        RBD.frame_after(joint0), 
+        com = [0.0, 0.0, 0.0], # center of mass location with respect to joint, should be all zero for the base
+        moment_about_com = model.body_inertias, # inertia matirx
+        mass = model.body_mass
+    )
+    base = RBD.RigidBody("base", base_inertia_frame) # define body "base" using frame "base_inertia_frame"
+    # this describes the position of joint 0 relative to the world
+    before_joint0_to_world = RBD.Transform3D(
+        RBD.frame_before(joint0), # frame before joint 0 should be world frame
+        RBD.default_frame(world), # world is a body, this gets the world's body frame
+        one(RotMatrix{3}), # now we just set rotation and translation as unit/zero
+        SVector{3}([0., 0., 0.])
+    )
+    # now we connect "base" to "world" through "joint0" which is located at "before_joint0_to_world"
+    RBD.attach!(FloatingSpaceRobot, world, base, joint0, joint_pose=before_joint0_to_world)
+    
+    """Adding arms"""
+    joint = Array{Joint{Float64,RigidBodyDynamics.Revolute{Float64}}}(UndefInitializer(), nb)
+    link = Array{RigidBodyDynamics.RigidBody{Float64}}(UndefInitializer(), nb)
+
+    # first arm linking to base
+    joint[1] = RBD.Joint(string("joint", 1), RBD.Revolute(model.joint_directions[1]))
+    inertia_frame = RBD.SpatialInertia(
+        RBD.frame_after(joint[1]), 
+        com = [model.arm_length/2, 0, 0], # center of mass location with respect to joint
+        # arm initialized at positive x direction
+        moment_about_com = model.arm_inertias, # inertia matirx
+        mass = model.arm_mass
+    )
+    link[1] = RBD.RigidBody(string("link", 1), inertia_frame)
+    joint_pose = RBD.Transform3D(
+            RBD.frame_before(joint[1]), 
+            RBD.frame_after(joint0), # base frame
+            one(RotMatrix{3}), 
+            SVector{3}([model.body_size/2, 0., 0.]) # joint1 is off by half of the body size to positive x axis
+        )
+    RBD.attach!(FloatingSpaceRobot, base, link[1], joint[1], joint_pose = joint_pose)
+
+    for idx = 2 : nb
+        joint[idx] = RBD.Joint(string("joint", idx), RBD.Revolute(model.joint_directions[idx]))
+        inertia_frame = RBD.SpatialInertia(
+            RBD.frame_after(joint[idx]), 
+            com = [model.arm_length/2, 0, 0], # center of mass location with respect to joint
+            # arm initialized at positive x direction
+            moment_about_com = model.arm_inertias, # inertia matirx
+            mass = model.arm_mass
+        )
+        link[idx] = RBD.RigidBody(string("link", idx), inertia_frame)
+        joint_pose = RBD.Transform3D(
+            RBD.frame_before(joint[idx]), 
+            RBD.frame_after(joint[idx - 1]), # base frame
+            one(RotMatrix{3}), 
+            SVector{3}([model.arm_length/2, 0., 0.]) # next joint is off by half of the arm length to positive x axis
+        )
+        RBD.attach!(FloatingSpaceRobot, link[idx-1], link[idx], joint[idx], joint_pose = joint_pose)
+    end
+    return FloatingSpaceRobot
+end
+FloatingSpaceRobot = generate_RBD_model(model::FloatingSpace)
+
 """Build visualizer"""
 # using MC implementation
-model = FloatingSpaceOrth(2)
 mech = vis_mech_generation(model)
 steps = Base.OneTo(Int(N))
 storage = CD.Storage{Float64}(steps,length(mech.bodies))
@@ -45,7 +117,7 @@ function generate_config_rc2mc(
     prev_q = UnitQuaternion(base_rotations...)
     state = [pin;base_v;RS.params(prev_q);base_ω]
 
-    """Arm links"""
+    """Arm link"""
     pin = pin+prev_q * [model.body_size/2,0,0]
     # find quaternion from joint angles
     rotations = []
@@ -76,6 +148,7 @@ base_ω0 = [0., 0., 0.]
 joint_angles0 = fill.(pi/13,model.nb)
 
 x0 = generate_config(model, [base_x0;pi/3], joint_angles0);
+# we are still not handling velocities well
 @test x0 ≈ generate_config_rc2mc(model, base_x0, base_q0, base_v0, base_ω0, joint_angles0)
 
 # Visualize initial condition
@@ -85,6 +158,7 @@ view_single_state(model, x0)
 U = 0.03*zeros(6+model.nb)
 U[5] = 1
 U[6] = 1
+@show U
 """MC simulation"""
 x = x0
 λ_init = zeros(5*model.nb)
@@ -107,108 +181,27 @@ end
 visualize(mech,storage, env = "editor")
 
 """RBD simulation"""
-# define zero gravity world
-world = RBD.RigidBody{Float64}("world")
-FloatingSpaceRobot = RBD.Mechanism(world; gravity=[0., 0., 0.])
+state = RBD.MechanismState(FloatingSpaceRobot)
+RBD.set_configuration!(state, joints(FloatingSpaceRobot)[1], base_q0) # set floating joint rotation
+RBD.set_velocity!(state, joints(FloatingSpaceRobot)[1], [base_ω0; base_v0])
 
-joint0 = RBD.Joint("joint0", RBD.QuaternionFloating{Float64}()) # joint connecting base to world
-
-# inertia frame of base
-base_inertia_frame = RBD.SpatialInertia(
-    RBD.frame_after(joint0), 
-    com = [0.0, 0.0, 0.0], # center of mass location with respect to joint, should be all zero for the base
-    moment_about_com = model.body_inertias, # inertia matirx
-    mass = model.body_mass
-)
-base = RBD.RigidBody("base", base_inertia_frame) # define body "base" using frame "base_inertia_frame"
-
-# this describes the position of joint 0 relative to the world
-before_joint0_to_world = RBD.Transform3D(
-    RBD.frame_before(joint0), # frame before joint 0 should be world frame
-    RBD.default_frame(world), # world is a body, this gets the world's body frame
-    one(RotMatrix{3}), # now we just set rotation and translation as unit/zero
-    SVector{3}([0., 0., 0.])
-)
-# now we connect "base" to "world" through "joint0" which is located at "before_joint0_to_world"
-RBD.attach!(FloatingSpaceRobot, world, base, joint0, joint_pose=before_joint0_to_world)
-
-@testset "simulate base" begin
-    state = RBD.MechanismState(FloatingSpaceRobot)
-    q0 = base_q0
-    v0 = [0., 0., 1.]
-    ω0 = [1., 0., 0.]
-    RBD.set_configuration!(state, joint0, q0) # set floating joint rotation
-    RBD.set_velocity!(state, joint0, [ω0; v0])
-    
-    ts, qs, vs = simulate(state, 1., Δt=0.001)
-    @test all(all(!isnan, q) for q in qs)
-    @test all(all(!isnan, v) for v in vs)
-    @test all(norm(v[4:6])≈ 1.0 for v in vs) 
+for idx = 1 : model.nb  
+    RBD.set_configuration!(state, joints(FloatingSpaceRobot)[idx+1], joint_angles0[idx])
+    RBD.set_velocity!(state, joints(FloatingSpaceRobot)[idx+1], 0)
 end
 
-# Add the first arm link
-joint1 = RBD.Joint("joint1", RBD.Revolute(model.joint_directions[1])) # joint1 rotate about axis "model.joint_directions[1]"
-link1_inertia_frame = RBD.SpatialInertia(
-    RBD.frame_after(joint1), 
-    com = [model.arm_length/2, 0, 0], # center of mass location with respect to joint
-    # arm initialized at positive x direction
-    moment_about_com = model.arm_inertias, # inertia matirx
-    mass = model.arm_mass
-)
-link1 = RBD.RigidBody("link1", link1_inertia_frame)
-
-before_joint1_to_base = RBD.Transform3D(
-    RBD.frame_before(joint1), 
-    RBD.frame_after(joint0), # base frame
-    one(RotMatrix{3}), 
-    SVector{3}([model.body_size/2, 0., 0.]) # joint1 is off by half of the body size to positive x axis
-)
-
-RBD.attach!(FloatingSpaceRobot, base, link1, joint1, joint_pose = before_joint1_to_base)
-
-# Add the second arm link
-joint2 = RBD.Joint("joint2", RBD.Revolute(model.joint_directions[2])) 
-link2_inertia_frame = RBD.SpatialInertia(
-    RBD.frame_after(joint2), 
-    com = [model.arm_length/2, 0, 0], # center of mass location with respect to joint
-    # arm initialized at positive x direction
-    moment_about_com = model.arm_inertias, # inertia matirx
-    mass = model.arm_mass
-)
-link2 = RBD.RigidBody("link2", link2_inertia_frame)
-before_joint2_to_after_joint1 = RBD.Transform3D(
-    RBD.frame_before(joint2), 
-    RBD.frame_after(joint1), # link1 frame
-    one(RotMatrix{3}), 
-    SVector{3}([model.body_size/2, 0., 0.])
-)
-
-RBD.attach!(FloatingSpaceRobot, link1, link2, joint2, joint_pose = before_joint2_to_after_joint1)
-
-# start simulation
-state = RBD.MechanismState(FloatingSpaceRobot)
-
-RBD.set_configuration!(state, joint0, base_q0) # set floating joint rotation
-RBD.set_velocity!(state, joint0, [base_ω0; base_v0])
-
-RBD.set_configuration!(state, joint1, joint_angles0[1])
-RBD.set_velocity!(state, joint1, 0)
-
-RBD.set_configuration!(state, joint2, joint_angles0[2])
-RBD.set_velocity!(state, joint2, 0)
-
-RBD.set_configuration!(state, RBD.configuration(state) .+ [zeros(4);base_x0;0.;0.])
+RBD.set_configuration!(state, RBD.configuration(state) .+ [zeros(4);base_x0;zeros(model.nb)])
 
 controller! = function controller!(τ, t, state)
-    τ .= 0.03*zeros(6+model.nb)
-    τ[2] = 1.
-    τ[3] = 1.
+    τ .= 0.03*zeros(6 + model.nb)
+    τ[2] = U[5]
+    τ[3] = U[6]
 end
 
 ts, qs, vs = RBD.simulate(state, Tf,controller!, Δt=dt)
 
 for idx = 1:N
-    current_state = generate_config_rc2mc(model, qs[idx][5:7], qs[idx][1:4], vs[idx][4:6], vs[idx][1:3], qs[idx][8:9])
+    current_state = generate_config_rc2mc(model, qs[idx][5:7], qs[idx][1:4], vs[idx][4:6], vs[idx][1:3], qs[idx][8:end])
     setStates!(model, mech, current_state)
     CD.discretizestate!(mech)
     for i=1:model.nb+1
@@ -219,4 +212,3 @@ for idx = 1:N
     end
 end
 visualize(mech, storage_RBD, env = "editor")
-
