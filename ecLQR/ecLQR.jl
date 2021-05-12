1aa78782aa785fe23814e2fab236993bde5a742a
Shuo <paulyang1990@gmail.com>
2021-05-12 16:03:08 -0400

    compare Jan's LQR and ecLQR_laine, actually Jan's LQR is better

--- ecLQR/ecLQR.jl
+++ ecLQR/ecLQR.jl
@@ -42,6 +42,8 @@ struct ecLQR{T, nx, nu, ncxu, ncxuN, N}
 
     Kx_list::Vector{SizedMatrix{nu,nx,T,2,Matrix{T}}}   # 1 --- N-1   
     kl_list::Vector{SizedVector{nu,T,Vector{T}}}       # 1 --- N-1
+    Kλ_list::Vector{SizedMatrix{ncxu,nx,T,2,Matrix{T}}}   # 1 --- N-1   
+    kλ_list::Vector{SizedVector{ncxu,T,Vector{T}}}       # 1 --- N-1
 
     # storage
     mx::SizedVector{nx,T,Vector{T}} 
@@ -71,6 +73,9 @@ struct ecLQR{T, nx, nu, ncxu, ncxuN, N}
         vx_list =  [SizedVector{nx}(zeros(T,nx)) for i=1:N]
         Kx_list =  [SizedMatrix{nu,nx}(zeros(T,nu,nx)) for i=1:N]
         kl_list =  [SizedVector{nu}(zeros(T,nu)) for i=1:N]
+        Kλ_list =  [SizedMatrix{ncxu,nx}(zeros(T,ncxu,nx)) for i=1:N]
+        kλ_list =  [SizedVector{ncxu}(zeros(T,ncxu)) for i=1:N]
+
         Hx_list = [ Matrix{T}(undef,nx,nx) for i = 1:N]
         hl_list = [ Vector{T}(undef,nx) for i = 1:N]
         contg = constraint_togo{T}(Hx_list, hl_list)
@@ -97,11 +102,65 @@ struct ecLQR{T, nx, nu, ncxu, ncxuN, N}
             Vxx_list, vx_list, 
             contg,
             Kx_list, kl_list,
+            Kλ_list, kλ_list,
             mx, mu, Mxx, Muu, Mux,
             Nx, Nu, nl, Py, Zw, yt, wt
         )
     end
   
+    function ecLQR{T}(nx,nu,ncxu,ncxuN,N) where T
+        Q_list = [SizedMatrix{nx,nx}(zeros(Float64,nx,nx)) for i=1:N]
+        q_list = [SizedVector{nx}(zeros(Float64,nx)) for i=1:N]
+        R_list = [SizedMatrix{nu,nu}(zeros(Float64,nu,nu)) for i=1:N-1]
+        r_list = [SizedVector{nu}(zeros(Float64,nu)) for i=1:N-1]
+        H_list = [SizedMatrix{nu,nx}(zeros(Float64,nu,nx)) for i=1:N-1]
+    
+        A_list = [SizedMatrix{nx,nx}(zeros(Float64,nx,nx)) for i=1:N]
+        B_list = [SizedMatrix{nx,nu}(zeros(Float64,nx,nu)) for i=1:N]
+        f_list = [SizedVector{nx}(zeros(Float64,nx)) for i=1:N]
+        C_list = [SizedMatrix{ncxu,nx}(zeros(Float64,ncxu,nx)) for i=1:N-1]
+        D_list = [SizedMatrix{ncxu,nu}(zeros(Float64,ncxu,nu)) for i=1:N-1]
+        g_list = [SizedVector{ncxu}(zeros(Float64,ncxu)) for i=1:N-1]
+        CN = SizedMatrix{ncxu,nx}(zeros(Float64,ncxu,nx))
+        gN = SizedVector{ncxu}(zeros(Float64,ncxu))
+
+        Vxx_list = [SizedMatrix{nx,nx}(zeros(T,nx,nx)) for i=1:N]
+        vx_list =  [SizedVector{nx}(zeros(T,nx)) for i=1:N]
+        Kx_list =  [SizedMatrix{nu,nx}(zeros(T,nu,nx)) for i=1:N]
+        kl_list =  [SizedVector{nu}(zeros(T,nu)) for i=1:N]
+        Kλ_list =  [SizedMatrix{ncxu,nx}(zeros(T,ncxu,nx)) for i=1:N]
+        kλ_list =  [SizedVector{ncxu}(zeros(T,ncxu)) for i=1:N]
+        Hx_list = [ Matrix{T}(undef,nx,nx) for i = 1:N]
+        hl_list = [ Vector{T}(undef,nx) for i = 1:N]
+        contg = constraint_togo{T}(Hx_list, hl_list)
+
+        mx = SizedVector{nx}(zeros(T,nx)) 
+        mu = SizedVector{nu}(zeros(T,nu)) 
+        Mxx = SizedMatrix{nx,nx}(zeros(T,nx,nx))
+        Muu = SizedMatrix{nu,nu}(zeros(T,nu,nu))
+        Mux = SizedMatrix{nu,nx}(zeros(T,nu,nx))
+
+        Nx = zeros(T, nx, nx)
+        Nu = zeros(T, nx, nx)
+        nl = zeros(T, nx)
+        Py = zeros(T, nx, nx)
+        Zw = zeros(T, nx, nx)
+        yt = zeros(T, nx)
+        wt = zeros(T, nx)
+
+
+        new{T, nx, nu, ncxu, ncxuN, N}(
+        Q_list, q_list, R_list, r_list, H_list,
+        A_list, B_list, f_list,
+        C_list, D_list, g_list, CN, gN,
+        Vxx_list, vx_list, 
+        contg,
+        Kx_list, kl_list,
+        Kλ_list, kλ_list,
+        mx, mu, Mxx, Muu, Mux,
+        Nx, Nu, nl, Py, Zw, yt, wt
+        )
+    end
 end
 
 function ecLQR_backward!(ec::ecLQR{T, nx, nu, ncxu, ncxuN, N}) where {T, nx, nu, ncxu, ncxuN, N}
@@ -177,4 +236,156 @@ function ecLQR_backward!(ec::ecLQR{T, nx, nu, ncxu, ncxuN, N}) where {T, nx, nu,
         ec.vx_list[i] .= ec.mx + ec.Kx_list[i]'*ec.mu + (ec.Mux'+ ec.Kx_list[i]'*ec.Muu)*ec.kl_list[i]
 
     end
+end
+
+# the same function, but use D E as input, return ΔV
+function ecLQR_backward!(ec::ecLQR{T, nx, nu, ncxu, ncxuN, N}, D, E) where {T, nx, nu, ncxu, ncxuN, N}
+    ΔV_1 = 0
+    ΔV_2 = 0
+    ec.Vxx_list[N] .= E[N].Q
+    ec.vx_list[N] .= E[N].q
+    # last time index no constraint (k=N)
+    ec.contg.Hx_list[N] = SizedMatrix{ncxu,nx}(zeros(Float64,ncxu,nx))
+    ec.contg.hl_list[N] = SizedVector{ncxu}(zeros(Float64,ncxu))
+
+    # notice the nu here is m+ncxu 
+    m = nu - ncxu
+    for i=N-1:-1:1
+        dyn_exp = D[i]
+        cost_exp = E[i]
+        # Q,q,R,r,H,c = cost_exp.Q,cost_exp.q,cost_exp.R,cost_exp.r,cost_exp.H,cost_exp.c
+        # A,B,C,G = dyn_exp.A, dyn_exp.B, dyn_exp.C, dyn_exp.G
+
+        Fxt = dyn_exp.A;
+        Fut = [dyn_exp.B dyn_exp.C];
+        Gxt = dyn_exp.G*dyn_exp.A;
+        Gut = [dyn_exp.G*dyn_exp.B dyn_exp.G*dyn_exp.C];
+        gl = SizedVector{ncxu}(zeros(Float64,ncxu))       
+
+        # equation 12
+        ec.mx .= cost_exp.q;
+        mul!(ec.mx, Transpose(Fxt), ec.vx_list[i+1], 1.0, 1.0)
+        ec.mu .= [cost_exp.r; zeros(ncxu)]; 
+        mul!(ec.mu, Transpose(Fut), ec.vx_list[i+1], 1.0, 1.0)
+        
+        Raug = zeros(nu,nu)
+        Raug[1:m,1:m] .= cost_exp.R
+
+        Haug = zeros(nu,nx)
+        Haug[1:m,:] = cost_exp.H
+
+        ec.Mxx .= cost_exp.Q + Fxt'*ec.Vxx_list[i+1]*Fxt;
+        ec.Muu .= Raug + Fut'*ec.Vxx_list[i+1]*Fut;
+        ec.Mux .= Haug + Fut'*ec.Vxx_list[i+1]*Fxt;
+
+        Nxt = [Gxt; ec.contg.Hx_list[i+1]*Fxt];
+        Nut = [Gut; ec.contg.Hx_list[i+1]*Fut]; 
+        nlt = [gl; ec.contg.Hx_list[i+1]*ec.f_list[i] + ec.contg.hl_list[i+1]];
+
+        # svd to get P and Z, equation 13d
+        U,S,V = svd(Nut, full=true);
+        rankNut = length(S[S .> 1e-6])
+        if (rankNut == 0)
+            Z = V;
+            A = Z'*ec.Muu*Z;
+            b = Z';
+            # equation 17 and 18
+            K = -( Z*(A\b)*ec.Mux );
+            k = -( Z*(A\b)*ec.mu );
+            ec.kl_list[i] .= k;
+            ec.Kx_list[i] .= K;
+        elseif (rankNut == nu)
+            P = V;
+            # equation 17 and 18
+            K = -( P*pinv(Nut*P)*Nxt);
+            k = -( P*pinv(Nut*P)*nlt);
+            ec.kl_list[i] .= k;
+            ec.Kx_list[i] .= K;
+        else
+            P = V[:,1:rankNut];
+            Z = V[:,rankNut+1:nu];
+            # equation 17 and 18
+            A = Z'*ec.Muu*Z;
+            b = Z';
+            K = -( P*pinv(Nut*P)*Nxt + Z*(A\b)*ec.Mux );
+            k = -( P*pinv(Nut*P)*nlt + Z*(A\b)*ec.mu );
+            ec.kl_list[i] .= k;
+            ec.Kx_list[i] .= K;
+        end
+
+        # remove redudant terms, the paragraph below equation 21
+        c = [ec.contg.hl_list[i+1] ec.contg.Hx_list[i+1]]
+        U,_,_ = svd(c)
+        c = U'*c
+        rows = size(c,1);
+        c = c[1:rows-rankNut,:]
+        ec.contg.hl_list[i] = c[:,1]
+        ec.contg.Hx_list[i] = c[:,2:end]
+
+        # update cost to go, equation 24 and 25
+        tmpmtx = ec.Mxx + 2*ec.Kx_list[i]'*ec.Mux + ec.Kx_list[i]'*ec.Muu*ec.Kx_list[i]
+        ec.Vxx_list[i] .= (tmpmtx + tmpmtx') /2
+        ec.vx_list[i] .= ec.mx + ec.Kx_list[i]'*ec.mu + (ec.Mux'+ ec.Kx_list[i]'*ec.Muu)*ec.kl_list[i]
+   
+        
+        ΔV_1 += ec.kl_list[i]' * ec.mu
+        ΔV_2 += 0.5*ec.kl_list[i]' * ec.Muu * ec.kl_list[i]
+    end
+    return ΔV_1, ΔV_2
+
+end
+
+# Follow Jan's 
+function ecLQR_backward_Jan!(ec::ecLQR{T, nx, nu, ncxu, ncxuN, N}, D, E) where {T, nx, nu, ncxu, ncxuN, N}    ΔV = 0
+    ΔV_1 = 0
+    ΔV_2 = 0
+    ec.Vxx_list[N] .= E[N].Q
+    ec.vx_list[N] .= E[N].q
+
+    n = nx
+    m = nu
+    p = ncxu
+    for i=N-1:-1:1
+        dyn_exp = D[i]
+        cost_exp = E[i]
+        Q,q,R,r,c = cost_exp.Q,cost_exp.q,cost_exp.R,cost_exp.r,cost_exp.c 
+        A,B,C,G = dyn_exp.A, dyn_exp.B, dyn_exp.C, dyn_exp.G
+
+
+        Dmtx = B - C/(G*C)*G*B
+        M11 = R + Dmtx'*ec.Vxx_list[i+1]*B
+        M12 = Dmtx'*ec.Vxx_list[i+1]*C
+        M21 = G*B
+        M22 = G*C
+
+        M = [M11 M12;M21 M22]
+        b = [Dmtx'*ec.Vxx_list[i+1];G]*A
+
+        K_all = M\b
+        Ku = K_all[1:m,:]
+        Kλ = K_all[m .+ (1:p),:]
+
+        l_all = M\[r + Dmtx'*ec.vx_list[i+1]; zeros(p)]
+        lu = l_all[1:m]
+        lλ = l_all[m .+ (1:p)]
+
+        ec.kl_list[i] .= -lu;
+        ec.Kx_list[i] .= -Ku;
+
+        ec.Kλ_list[i] .= -Kλ;
+        ec.kλ_list[i] .= -lλ;
+
+        Abar = A -B*Ku -C*Kλ
+        bbar = -B*lu 
+
+        t1 = -lu'*r -ec.vx_list[i+1]'*(B*lu)
+        t2 = 0.5*lu'*R*lu + lu'*(B'*ec.Vxx_list[i+1]*B)*lu
+
+        ec.Vxx_list[i] .= Q + Ku'*R*Ku + Abar'*ec.Vxx_list[i+1]*Abar
+        ec.vx_list[i] .= q - Ku'*r + Ku'*R*lu + Abar'*ec.Vxx_list[i+1]*bbar + Abar'*ec.vx_list[i+1]
+
+        ΔV_1 += t1
+        ΔV_2 += t2
+    end
+    return ΔV_1, ΔV_2
 end
---
+++ linearize_lqr_floatingBaseSpace.jl
@@ -0,0 +1,209 @@
+# in this script, we first simulate the floatingBaseSpace system, then we linearize the system dynamics 
+# to get A B C G, then we calculate the K and d for this linearized problem. 
+# basically this is a simplifed version of the inner loop of an iLQR algorithm
+
+include("floatingBaseSpace.jl")
+include("ecLQR/ecLQR.jl")
+
+function simulate_model(model)
+    n,m = size(model)
+    np = model.p   # constraint size
+    n̄ = RD.state_diff_size(model)
+
+    x0 = generate_config(model, [0.0;0.0;1.0;pi/2], fill.(pi/4,model.nb));
+    xf = generate_config(model, [0.3;0.3;1.0;pi/6], fill.(pi/6,model.nb));
+    Tf = 1
+    dt = 0.01
+    N = Int(Tf/dt) 
+    x_list = [SizedVector{n}(zeros(Float64,n)) for i=1:N]
+    x_list[1] .= x0
+    x = x0
+
+    λ_list = [SizedVector{np}(zeros(Float64,np)) for i=1:N-1]
+    λ_init = zeros(5*model.nb)
+    λ = λ_init
+    λ_list[1] .= λ_init
+    
+    u_list = [SizedVector{m}(zeros(Float64,m)) for i=1:N-1]
+    U = 0.03*zeros(m)
+    println("start to simulate")
+    @time begin
+        for idx = 2:N
+            println("step: ",idx)
+            U .= 0.03*zeros(m)
+            u_list[idx-1] .= U
+            x1, λ1 = discrete_dynamics(model,x, U, λ, dt)  # solved x1 is the state at t = idx
+            x .= x1
+            x_list[idx] .= x1
+            λ .= λ1
+            λ_list[idx-1] .= λ1   # solved λ1 is the λ at t= idx-1
+        end 
+    end
+    # return x_list, u_list, λ_list
+
+
+    # we have x_list, u_list, λ_list now 
+    # next we need tools in TrajectoryOptimization to get some expansion data structure 
+    # objective
+    Qf = Diagonal(@SVector fill(550., n))
+    Q = Diagonal(@SVector fill(1e-2, n))
+    R = Diagonal(@SVector fill(1e-3, m))
+    costfuns = [TO.LieLQRCost(RD.LieState(model), Q, R, SVector{n}(xf); w=1e-1) for i=1:N]
+    costfuns[end] = TO.LieLQRCost(RD.LieState(model), Qf, R, SVector{n}(xf); w=550.0)
+    obj = Objective(costfuns);
+
+    # state difference
+    Gjacob = [SizedMatrix{n,n̄}(zeros(n,n̄)) for k = 1:N+1]
+
+    # expand cost function along the trajectory 
+    # function TO.cost_expansion!(E::Objective, obj::Objective, Z::Traj, init::Bool=false, rezero::Bool=false)
+    E = TO.QuadraticObjective(n̄,m,N)
+	quad_exp = TO.QuadraticObjective(E, model)    # TO/src/objective.jl  create an objective with n,m
+
+    # construct TO.Traj, follows TO/src/Problem.jl
+    dt_list = fill(dt, N)
+    t = pushfirst!(cumsum(dt_list), 0)
+    Z = Traj(x_list,u_list,dt_list,t)
+
+    TO.cost!(obj, Z)
+    _J = TO.get_J(obj)
+    println("Init cost is:", sum(_J))
+
+    # following are from Altro/stc/ilqr/ilqr.jl
+    # and 
+    # Altro/src/ilqr/ilqr_solve.jl
+    D = [TO.DynamicsExpansionMC(model) for k = 1:N-1]
+    Λ = λ_list
+    TO.dynamics_expansion!(RK4, D, model, Z, Λ)
+    TO.state_diff_jacobian!(Gjacob, model, Z)
+    TO.cost_expansion!(quad_exp, obj, Z, false, true)
+    TO.error_expansion!(E, quad_exp, model, Z, Gjacob)
+
+	# cost_exp = solver.E[k]
+	# dyn_exp = solver.D[k]
+    #    S⁺, s⁺ = S.Q, S.q
+    # Q,q,R,r,H,c = cost_exp.Q,cost_exp.q,cost_exp.R,cost_exp.r,cost_exp.H,cost_exp.c
+    # A,B,C,G = dyn_exp.A, dyn_exp.B, dyn_exp.C, dyn_exp.G
+
+    α = 1.0/32
+    # combine u and lambda together as input
+    ec_uλ = ecLQR{Float64}(n̄, m+np, np, np, N)
+    # solve du and K d using ecLQR 
+    ΔV_1, ΔV_2 = ecLQR_backward!(ec_uλ, D, E)
+    println("ec_uλ | ΔV :", -α*ΔV_1)
+    # test ec_uλ
+    constraint_vio = 0
+    for idx= 1:N-1
+        dyn_exp = D[idx]
+        A,B,C,G = dyn_exp.A, dyn_exp.B, dyn_exp.C, dyn_exp.G
+        δx = 0.01*randn(n̄)
+        δu = ec_uλ.kl_list[idx][1:m] 
+        mul!(δu, ec_uλ.Kx_list[idx][1:m,:], δx, 1.0, 1.0)
+        δλ = ec_uλ.kl_list[idx][m+1:end] 
+        mul!(δλ, ec_uλ.Kx_list[idx][m+1:end,:], δx, 1.0, 1.0)
+        constraint_vio += norm(G*(A*δx + B*δu + C*δλ))
+    end
+    println("ec_uλ | total constraint vio:", constraint_vio)
+
+
+    # update using ec_uλ
+    ec = ec_uλ
+
+    # : rollout again, check dJ
+    Z̄ = Traj(x_list,u_list,dt_list,t)
+    Z̄[1].z = [x0; control(Z[1])]
+    δx = zeros(n̄)
+    δu = zeros(m)
+    Λ2 = λ_list
+
+    for k = 1:N-1
+        δx .= RobotDynamics.state_diff(model, state(Z̄[k]), state(Z[k]))
+        δu = α*ec.kl_list[k][1:m] 
+        mul!(δu, ec.Kx_list[k][1:m,:], δx, 1.0, 1.0)
+        # δλ = α*ec.kl_list[k][m+1:end]
+        # mul!(δλ, ec.Kx_list[k][m+1:end,:], δx, 1.0, 1.0)
+        ū = control(Z[k]) + δu
+        # Λ2[k] = λ_list[k] + δλ
+        # ū = control(Z[k])
+        RobotDynamics.set_control!(Z̄[k], ū)
+        x⁺, Λ2[k] = discrete_dynamics(model,state(Z̄[k]), control(Z̄[k]), λ_list[k], Z̄[k].dt)
+        actual_δλ = Λ2[k] - λ_list[k]
+        Z̄[k+1].z = [x⁺; control(Z[k+1])]
+
+        # check δ terms
+
+        # dyn_exp = D[k]
+        # A,B,C,G = dyn_exp.A, dyn_exp.B, dyn_exp.C, dyn_exp.G
+        # println(norm(G*(A*δx + B*δu + C*actual_δλ)))
+
+    end
+
+    TO.cost!(obj, Z̄)
+    _J = TO.get_J(obj)
+    println("after update using ec_uλ, cost is:", sum(_J))
+
+
+
+    ec_u = ecLQR{Float64}(n̄, m, np, np, N)
+    ΔV_1, ΔV_2 = ecLQR_backward_Jan!(ec_u, D, E)
+    println("ec_u | ΔV :", -α*ΔV_1 )
+    # test ec_u
+    constraint_vio = 0
+    for idx= 1:N-1
+        dyn_exp = D[idx]
+        A,B,C,G = dyn_exp.A, dyn_exp.B, dyn_exp.C, dyn_exp.G
+        δx = 0.01*randn(n̄)
+        δu = ec_u.kl_list[idx] 
+        mul!(δu, ec_u.Kx_list[idx], δx, 1.0, 1.0)
+        δλ = ec_u.kλ_list[idx] 
+        mul!(δλ, ec_u.Kλ_list[idx], δx, 1.0, 1.0)
+        constraint_vio += norm(G*(A*δx + B*δu + C*δλ))
+    end
+    println("ec_u | total constraint vio:", constraint_vio)
+    # update using ec_u
+    ec = ec_u
+
+    # : rollout again, check dJ
+    Z̄ = Traj(x_list,u_list,dt_list,t)
+    Z̄[1].z = [x0; control(Z[1])]
+	δx = zeros(n̄)
+	δu = zeros(m)
+    Λ2 = λ_list
+
+    for k = 1:N-1
+        δx .= RobotDynamics.state_diff(model, state(Z̄[k]), state(Z[k]))
+        δu .= α*ec.kl_list[k][1:m] 
+        mul!(δu, ec.Kx_list[k][1:m,:], δx, 1.0, 1.0)
+        # δλ = α*ec.kλ_list[k]
+        # mul!(δλ, ec.Kλ_list[k], δx, 1.0, 1.0)
+        ū = control(Z[k]) + δu
+        # Λ2[k] = λ_list[k] + δλ
+        # ū = control(Z[k])
+        RobotDynamics.set_control!(Z̄[k], ū)
+        x⁺, Λ2[k] = discrete_dynamics(model,state(Z̄[k]), control(Z̄[k]), λ_list[k], Z̄[k].dt)
+        actual_δλ = Λ2[k] - λ_list[k]
+        Z̄[k+1].z = [x⁺; control(Z[k+1])]
+
+        # check δ terms
+
+        # dyn_exp = D[k]
+        # A,B,C,G = dyn_exp.A, dyn_exp.B, dyn_exp.C, dyn_exp.G
+        # println(norm(G*(A*δx + B*δu + C*actual_δλ)))
+
+    end
+
+    TO.cost!(obj, Z̄)
+    _J = TO.get_J(obj)
+    println("after update using ec_u, cost is:", sum(_J))
+
+
+
+    # # compare the ΔV of the two cases
+    return ec_uλ, ec_u
+end
+
+
+
+
+model = FloatingSpaceOrth(2)
+ec_uλ, ec_u = simulate_model(model)
